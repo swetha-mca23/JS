d927e69f1fd8e952d5e260e91b73861ace0120d0
swetha <139735742+swetha-mca23@users.noreply.github.com>
2025-03-27 16:38:45 +0530

    Update array.js

--- js/Array/array.js
+++ js/Array/array.js
@@ -43,7 +43,7 @@ let students = [
 
 students.sort((a,b)=>{
     if(a.name < b.name) return -1
-    if(b.name < b.name) return 1
+    if(b.name < a.name) return 1
     return 0
 })
 console.log(students)
